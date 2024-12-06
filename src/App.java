7e5226e35f68c90f0a6c10abbc8d3dd00c9e64b8
Vedant R <110495968+Blue-Man-08@users.noreply.github.com>
2024-12-06 22:05:02 +0000

    Test commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args)
     {
-        //TestSuite.run();
+        TestSuite.run();
         Book randj = new Book();
         randj.readFromUrl("Romeo and Juliet", "https://www.gutenberg.org/cache/epub/1513/pg1513.txt");
         randj.writeToFile("omeoRay nday ulietJay.txt", 0, randj.getLineCount());
